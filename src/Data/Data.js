7e91a6b7bae8285eb20282d65f4525ba42a9d9c9
Kiran16k <kirankool16@gmail.com>
2023-09-14 17:09:43 +0530

    Honeycomb

---
+++ src/Data/Data.js
@@ -0,0 +1,202 @@
+export const randomData = [
+  {
+    name: "John Smith",
+    title: "Software Engineer",
+
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Emily Johnson",
+    title: "Graphic Designer",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Michael Brown",
+    title: "Data Analyst",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Sophia Lee",
+    title: "Marketing Manager",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "William Davis",
+    title: "Product Manager",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Olivia Wilson",
+    title: "UX Designer",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "James Taylor",
+    title: "Sales Representative",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Emma Anderson",
+    title: "HR Specialist",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Oliver Quinn",
+    title: "Financial Analyst",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Ava White",
+    title: "Content Writer",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Daniel Harris",
+    title: "Project Manager",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Mia Clark",
+    title: "UI Developer",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Elijah Lewis",
+    title: "Accountant",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Grace Turner",
+    title: "Media Manager",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "John Smith",
+    title: "Software Engineer",
+
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Emily Johnson",
+    title: "Graphic Designer",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Michael Brown",
+    title: "Data Analyst",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Sophia Lee",
+    title: "Marketing Manager",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "William Davis",
+    title: "Product Manager",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Olivia Wilson",
+    title: "UX Designer",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "James Taylor",
+    title: "Sales Representative",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  {
+    name: "Emma Anderson",
+    title: "HR Specialist",
+    image:
+      "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  },
+  //   {
+  //     name: "Emily Johnson",
+  //     title: "Graphic Designer",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Michael Brown",
+  //     title: "Data Analyst",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Sophia Lee",
+  //     title: "Marketing Manager",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "William Davis",
+  //     title: "Product Manager",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Olivia Wilson",
+  //     title: "UX Designer",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "James Taylor",
+  //     title: "Sales Representative",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Emma Anderson",
+  //     title: "HR Specialist",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Oliver Quinn",
+  //     title: "Financial Analyst",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Ava White",
+  //     title: "Content Writer",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Daniel Harris",
+  //     title: "Project Manager",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+  //   {
+  //     name: "Olivia Wilson",
+  //     title: "UX Designer",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgd_i4cU5x3PwX4XewrsI0OhFKUq0gkBf4g&usqp=CAU",
+  //   },
+];
---
+++ src/components/Imageoverlaytext.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Image.css";
+import { randomData } from "../Data/Data";
+
+const InstaCards = () => {
+  const rows = [];
+  let counter = 0;
+  let currentRow = [];
+
+  randomData.forEach((img, index) => {
+    let imagesPerRow = counter % 2 === 0 ? 6 : 5;
+    currentRow.push(
+      <div className="container" key={index}>
+        <div className="content">
+          <div className="content-overlay"></div>
+          <img
+            className="content-image"
+            src={img.image}
+            alt={`Img Post ${index}`}
+            height={"140px"}
+          />
+          <div className="content-details fadeIn-top">
+            <h3 className="content-text1">{img.name}</h3>
+          </div>
+          <div className="content-details fadeIn-bottom">
+            <h3 className="content-text2">{img.title}</h3>
+          </div>
+        </div>
+      </div>
+    );
+
+    if (currentRow.length === imagesPerRow || index === randomData.length - 1) {
+      rows.push(
+        <div className="row" key={index}>
+          {currentRow}
+        </div>
+      );
+      currentRow = [];
+      counter++;
+    }
+  });
+
+  return <div className="img-wrapper">{rows}</div>;
+};
+
+export default InstaCards;
